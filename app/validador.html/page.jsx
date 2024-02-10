e3fe9c939a40bda5a83b804c28f9d2c399329e7c
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-02-10 19:29:52 -0300

    atualiza

---
+++ app/validador.html/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+export default function Validador() {
+    const redireciona = () => {
+        location.href = 'https://dapper-panda-e60101.netlify.app/validador.html'
+    }
+    redireciona()
+    return (
+        <>
+            <h2>Carregando...</h2>        
+        </>
+    )
+}
