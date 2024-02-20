0779e63094501e0723906012f5fcfe1055ea9002
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-02-20 12:32:58 -0300

    arquivos Faculdade

--- app/components/inputs/InputLogin.jsx
+++ app/components/inputs/InputLogin.jsx
@@ -1,9 +1,11 @@
 'use client'
+import { FaRegEye } from "react-icons/fa";
 import { useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 export default function InputLogin() {
     const [passw, setPassw] = useState('')
     const [inco, setInco] = useState('')
+    const [ver, setVer] = useState('password')
     const router = useRouter()
     const pathName = usePathname()
 
@@ -21,14 +23,17 @@ export default function InputLogin() {
         <>
             <div className="mt-2">
                 {/* <label className="block text-sm text-white" >Insira a senha</label> */}
-                <input onChange={handleLoad} placeholder="Insira a Senha" className="w-full px-5 py-1 text-black-400 bg-gray-300 rounded focus:outline-none  focus:bg-white" type="password" />
+                <div className="flex items-center w-full px-5 py-1 text-black-400 bg-gray-300 rounded focus:outline-none  focus:bg-white	">
+                    <input onChange={handleLoad} placeholder="Insira a Senha" className="w-full px-5 py-1 text-black-400 bg-gray-300 rounded focus:outline-none  focus:bg-white" type={ver} />
+                    <FaRegEye className="hover:cursor-pointer" onClick={() => setVer('text')} />
+                </div>
             </div>
             <div className="mt-4 items-center flex justify-between">
                 <button className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 hover:bg-gray-800 rounded"
                     onClick={entrar} >Entrar</button>
             </div>
             <div>
-                <div className="bg-white p-1 my-5 rounded-md">
+                <div className="bg-white  my-5 rounded-md">
                     {inco}
                 </div>
             </div>
--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
