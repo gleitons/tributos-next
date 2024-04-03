3ebe1e032b05046e4c9486fbb55ead0a6a74b4a2
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-04-03 19:13:26 -0300

    ola

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,20 @@
+'use client'
+import { useState } from "react";
 import Login from "./components/login/Login";
+
 export default function Home() {
   
+  const [lagoaS, setLagoa] = useState('lagoa h-screen font-sans  bg-cover')
+
+  setTimeout(() => {
+    setLagoa('lagoa2 h-screen font-sans  bg-cover')
+  }, 1000);
+
 
 
   return (
     <div >
-      <div className="h-screen font-sans lagoa bg-cover" id="login">
+      <div className={lagoaS} id="login">
       <Login />
       </div>
 
