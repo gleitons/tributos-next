90e9cf871e21bcbe8e20408cfa86cafe77367bf4
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-04-04 16:22:12 -0300

    ola

--- app/components/botoes/BotaoAutentica.jsx
+++ app/components/botoes/BotaoAutentica.jsx
@@ -1,13 +1,25 @@
 "use client"
 import { useRouter } from "next/navigation"
+import LinkValidador from "./LinkValidador" 
+
 export default function BotaoAutentica() {
+   
     const route = useRouter()
-    const validador = () => {   
-        route.push('https://dapper-panda-e60101.netlify.app/validador.html')
+    const link = LinkValidador()   
+    function validador(link) {                
+        route.push(link)
+        document.querySelector('#carrega').classList.remove('hidden')
+    // onClick={validador(link)}
     }
     return (
         <>
-        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={validador}>Verificador online de Contratos</button>
+                
+        <span ><button onClick={() => validador(link)}  type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" >Verificador Online de Contratos</button>
+        <div className="hidden" id="carrega">
+            Carregando...
+        </div>
+        </span>
+        
         </>
     )
 }
---
+++ app/components/botoes/LinkValidador.jsx
@@ -0,0 +1,4 @@
+export default function LinkValidador() {
+    return "https://dapper-panda-e60101.netlify.app/validador.html"
+    // "https://dapper-panda-e60101.netlify.app/validador.html"
+};
