f3ac2e993220e2dde9aa640d71d472fa4c3c9e43
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2024-04-05 19:26:29 -0300

    atualização loading

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,4 +1,5 @@
 import BotaoAutentica from "./components/botoes/BotaoAutentica"
+import Image from "next/image"
 import Validador from "./validador/page"
 export default function NotFound() {
 
@@ -8,6 +9,7 @@ export default function NotFound() {
             <h3>Selecione a Opção</h3>
             <BotaoAutentica />
             <div className="invisible">
+            <Image src="https://dapper-panda-e60101.netlify.app/src/imagens/aviso-importante.jpeg" width={360} height={150} alt="Insira um numero"/>
             <Validador />
             </div>
         </div>
