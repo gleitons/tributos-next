e43d97ffee3080d2c9883a05665378d565d2c1a9
gleitons <gleitons@icloud.com>
2024-10-22 12:52:46 -0300

    ola

--- app/page.jsx
+++ app/page.jsx
@@ -1,37 +1,18 @@
-'use client'
 import Login from "./components/login/Login";
-import { useState, useEffect } from "react";
+
 
 export default function Home() {
-  const [backgroundPosition, setBackgroundPosition] = useState(0);
-  const [backgroundDiagonal, setbackgroundDiagonal] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setBackgroundPosition((prevPos) => prevPos + 1); // Incrementa a posição de fundo
-    }, 20);
-
-    const intervalDiagonal = setInterval(() => {
-      setbackgroundDiagonal((prevPos) => prevPos + 1); // Incrementa a posição de fundo
-    }, 20);
-
-    return () => clearInterval(interval, intervalDiagonal); // Limpa o intervalo quando o componente desmonta
-  }, []); // [] garante que o efeito seja executado uma vez após a renderização inicial
-
+  
   return (
-    <div>
-      <div
-        className="lagoa8 h-screen font-sans bg-cover"
-        id="login"
-        style={{
-          backgroundPosition: `-${backgroundPosition}px -${backgroundDiagonal}px`, // Atualiza o backgroundPosition dinamicamente
-          // backgroundImage: "url('/path-to-your-image.jpg')", // Substitua pela sua imagem
-        }}
-      >
+    <div >
+      
+      <div className={`lagoa8 h-screen font-sans  bg-cover`} id="login">   
         <div className="w-full h-screen bgEscuro">
-          <Login />
-        </div>
+        <Login />
+        </div>   
+      
       </div>
+
     </div>
   );
 }
