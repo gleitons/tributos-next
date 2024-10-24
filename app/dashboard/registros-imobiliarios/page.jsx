9ca8f2375090d9e7b885ea4ef2ef4fb09971a0eb
gleitons <gleitons@icloud.com>
2024-10-24 08:17:00 -0300

    limpa

--- app/dashboard/registros-imobiliarios/page.jsx
+++ app/dashboard/registros-imobiliarios/page.jsx
@@ -1,24 +1,25 @@
-'use client';
-import { useEffect, useState } from 'react';
+// 'use client';
+// import { useEffect, useState } from 'react';
 
 function MyComponent() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch('/api/imoveis')
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error('Erro ao carregar CSV', error));
-  }, []);
-  console.log(data.split('\n'));
+  // useEffect(() => {
+  //   fetch('/api/imoveis')
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data))
+  //     .catch((error) => console.error('Erro ao carregar CSV', error));
+  // }, []);
+  
   return (
     <div>
-      {data.map((row, index) => (
+      <p>Em construção</p>
+      {/* {data.map((row, index) => (
         <div key={index}>
            
           {Object.values(row).join(', ')} 
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
