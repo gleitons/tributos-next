8875de12eea048b29ee063b4f2516f7acf15dc7d
gleitons <gleitons@icloud.com>
2024-10-26 11:26:23 -0300

    ola

--- app/api/imoveis/route.js
+++ app/api/imoveis/route.js
@@ -1,23 +1,29 @@
+// app/api/imoveis/route.js
 import fs from 'fs';
 import path from 'path';
 import csv from 'csv-parser';
 
 export async function GET() {
-    const results = [];
-    const filePath = path.join(process.cwd(), 'app', 'ants', 'imoveis.csv'); // Caminho do arquivo
+  const results = [];
+  const filePath = path.join(process.cwd(), 'app', 'ants', 'imoveis.csv'); // Caminho do arquivo
 
-    // Usando Promessa para garantir que o CSV seja lido corretamente antes de retornar a resposta
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => {
-                results.push(data)                
-            })
-            .on('end', () => {
-                resolve(new Response(JSON.stringify(results), { status: 200 })); // Corrigido: Removido parêntese extra
-            })
-            .on('error', (error) => {
-                reject(new Response(JSON.stringify({ error: 'Erro ao processar CSV' }), { status: 500 }));
-            });
-    });
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv({ separator: ';' })) // Define o delimitador como ponto e vírgula
+      .on('data', (data) => {
+        results.push(data);
+      })
+      .on('end', () => {
+        resolve(new Response(JSON.stringify(results), {
+          status: 200,
+          headers: { 'Content-Type': 'application/json' }
+        }));
+      })
+      .on('error', (error) => {
+        reject(new Response(JSON.stringify({ error: 'Erro ao processar CSV' }), {
+          status: 500,
+          headers: { 'Content-Type': 'application/json' }
+        }));
+      });
+  });
 }
--- app/dashboard/registros-imobiliarios/page.jsx
+++ app/dashboard/registros-imobiliarios/page.jsx
@@ -1,27 +1,64 @@
-// 'use client';
-// import { useEffect, useState } from 'react';
+// app/dashboard/registros-imobiliarios/page.jsx
+import React from 'react';
 
-function MyComponent() {
-  // const [data, setData] = useState([]);
+async function fetchImoveisData() {
+  // const baseUrl = process.env.VERCEL_URL 
+  //   ? `https://${process.env.VERCEL_URL}` 
+  //   : 'http://localhost:3000'; // Ajuste a porta conforme seu ambiente de desenvolvimento
 
-  // useEffect(() => {
-  //   fetch('/api/imoveis')
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data))
-  //     .catch((error) => console.error('Erro ao carregar CSV', error));
-  // }, []);
-  
+  //   console.log('baseUrl:', baseUrl);
+  const res = await fetch(`http://localhost:3000/api/imoveis`);
+  if (!res.ok) {
+    throw new Error('Erro ao carregar dados');
+  }
+  return res.json();
+}
+
+export default async function RegistrosImobiliariosPage() {
+  const data = await fetchImoveisData();
+
+  // console.log(data[62])
   return (
     <div>
-      <p>Em construção</p>
-      {/* {data.map((row, index) => (
-        <div key={index}>
-           
-          {Object.values(row).join(', ')} 
-        </div>
-      ))} */}
+      <p>Imovis Cadastrados: {data.length}</p>
+      <div>
+        {/* <button onClick={() => data()}>Carregar</button> */}
+        {/* <button>Proximo</button> */}
+      </div>
+      <div className='overflow-auto h-screen'>
+        <table className='min-w-full border-collapse border border-gray-200'>
+          <thead className='bg-gray-300 sticky top-0 z-10'>
+            <tr className='bg-gray-300'>
+              <th className='p-2 border border-gray-200'>Imóvel</th>
+              <th className='p-2 border border-gray-200'>Proprietário</th>
+              <th className='p-2 border border-gray-200'>Setor</th>
+              <th className='p-2 border border-gray-200'>Quadra</th>
+              <th className='p-2 border border-gray-200'>Lote</th>
+              <th className='p-2 border border-gray-200'>Endereço</th>
+              <th className='p-2 border border-gray-200'>Área Total</th>
+              <th className='p-2 border border-gray-200'>Frente</th>
+            </tr>
+          </thead>
+          <tbody className='bg-slate-300 overflow-auto h-screen'>
+            {data.map((e, index) => (
+              <tr
+                key={index}
+                className={`hover:bg-slate-400 ${index % 2 === 0 ? 'bg-slate-200' : 'bg-slate-300'}`}
+              >
+                <td className='p-2 border border-gray-200'>{e.matricula} - {index}</td>
+                <td className='p-2 border border-gray-200'>{e.proprietario}</td>
+                <td className='p-2 border border-gray-200'>{e.codigo_setor}</td>
+                <td className='p-2 border border-gray-200'>{e.codigo_quadra}</td>
+                <td className='p-2 border border-gray-200'>{e.codigo_lote}</td>
+                <td className='p-2 border border-gray-200'>{`${e.rua_nome}, ${e.construcao_numero} - ${e.bairro_descricao}`}</td>
+                <td className='p-2 border border-gray-200'>{e.lote_area}</td>
+                <td className='p-2 border border-gray-200'>{e.valor_testada_lote}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
     </div>
   );
 }
-
-export default MyComponent;
