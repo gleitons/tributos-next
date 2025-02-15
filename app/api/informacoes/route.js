ef7f9532e7a3c9d65c9ee012ed0ec808e79b68d7
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2025-02-15 08:59:17 -0300

    ola

--- app/api/informacoes/route.js
+++ app/api/informacoes/route.js
@@ -2,10 +2,15 @@ import { NextResponse } from "next/server";
 
     
 export async function GET() {
+    
+    try {
+        
+    } catch (error) {
+        
+    }
     const fetchDados = async () => {
-        const url = 'https://script.google.com/macros/s/AKfycbwLdkjCEZAbCoFaWX7sfqjUSk3UL-hGdj0suHhtKRC1k1GBdsV7gyIISyQvyz9IpI63UA/exec';
-        const resp = await fetch(url);
-        const data = await resp.json();
+     
+       
        
         return data;
     }
--- app/dashboard/dados/page.jsx
+++ app/dashboard/dados/page.jsx
@@ -2,52 +2,53 @@ import Link from "next/link";
 import VerEmpresa from "../components/VerEmpresa";
 
 const importCadastros = async () => {
-  
-  try {
-    const resp = await fetch(`https://tributos.netlify.app/api/informacoes`, { revalidade: 10 });
-    const data = await resp.json();
-    data.sort((a, b) => a.empresa.localeCompare(b.empresa));
+    const url = 'https://script.google.com/macros/s/AKfycbwLdkjCEZAbCoFaWX7sfqjUSk3UL-hGdj0suHhtKRC1k1GBdsV7gyIISyQvyz9IpI63UA/exec';
+    try {
+        const resp = await fetch(url, { cache: 'no-store' });
 
-    return resp.ok ? data : [];
-  } catch (error) {
-    console.error("Erro ao solicitar:", error);
-    return [];
-  }
+        const data = await resp.json();
+        data.sort((a, b) => a.empresa.localeCompare(b.empresa));
+        return resp.ok ? data : [];
+
+    } catch (error) {
+        console.error("Erro ao solicitar:", error);
+        return [];
+    }
 };
 
 export default async function Page() {
-  const cadastro = await importCadastros();
+    const cadastro = await importCadastros();
 
-  return (
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <h2 className="text-2xl font-bold text-gray-800 text-center">
-        Dados Fiscais de Contribuintes
-      </h2>
-      <p className="text-center text-gray-600 text-sm">
-        Sala Mineira do Empreendedor
-      </p>
+    return (
+        <div className="p-6 bg-gray-100 min-h-screen">
+            <h2 className="text-2xl font-bold text-gray-800 text-center">
+                Dados Fiscais de Contribuintes
+            </h2>
+            <p className="text-center text-gray-600 text-sm">
+                Sala Mineira do Empreendedor
+            </p>
 
-      {/* Seção de Cadastro */}
-      <div className="mt-4 flex justify-center">
-        <Link
-          href="https://docs.google.com/spreadsheets/d/12BlFnJ-jdrLi_JQPBYvHervxePaX5lHMEjIh4eDVkXQ/edit?gid=0#gid=0"
-          target="_blank"
-          className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition"
-        >
-          ➕ Novo Cadastro
-        </Link>
-      </div>
+            {/* Seção de Cadastro */}
+            <div className="mt-4 flex justify-center">
+                <Link
+                    href="https://docs.google.com/spreadsheets/d/12BlFnJ-jdrLi_JQPBYvHervxePaX5lHMEjIh4eDVkXQ/edit?gid=0#gid=0"
+                    target="_blank"
+                    className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition"
+                >
+                    ➕ Novo Cadastro
+                </Link>
+            </div>
 
-      {/* Lista de Empresas */}
-      <div className="mt-6 max-h-[450px] overflow-auto bg-white shadow-md rounded-lg p-4">
-        <ul className="divide-y divide-gray-200 pb-80">
-          {cadastro.length > 0 ? (
-            cadastro.map((e, index) => <VerEmpresa key={index} dadosEmpresa={e} />)
-          ) : (
-            <p className="text-center text-gray-500 py-4">Nenhuma empresa encontrada.</p>
-          )}
-        </ul>
-      </div>
-    </div>
-  );
+            {/* Lista de Empresas */}
+            <div className="mt-6 max-h-[450px] overflow-auto bg-white shadow-md rounded-lg p-4">
+                <ul className="divide-y divide-gray-200 pb-80">
+                    {cadastro.length > 0 ? (
+                        cadastro.map((e, index) => <VerEmpresa key={index} dadosEmpresa={e} />)
+                    ) : (
+                        <p className="text-center text-gray-500 py-4">Nenhuma empresa encontrada.</p>
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
 }
